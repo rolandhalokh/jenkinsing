eb8f13108f1b0e50dca7371064ada41197789f29
rolandhalo <roland.halo@karhoo.com>
2018-09-14 14:31:49 +0100

    employee class

--- src/org/conf/buildUtils.groovy
+++ src/org/conf/buildUtils.groovy
@@ -1,24 +1,7 @@
 package org.demo
 
-@NonCPS
-class Student {
-   int StudentID;
-   String StudentName;
-   } 
-}
-
-
-String generatePodname(String source) {
-    // Convert percent-encoded characters to regular UTF-8 characters
-    source = URLDecoder.decode(source, "UTF-8")
-    String randomString = (
-        (1..4).inject("") {
-            a, b -> a += ('a'..'z')[
-                new Random().nextFloat() * 26 as int
-            ]
-        }
-    )
-    String jobname = "minion-" + randomString + "-" + source.replaceAll("[^-A-Za-z0-9_]", "-")
-    jobname = jobname.take(63).replaceAll("-+\$", "")
-    return jobname
+class Employee{
+    String firstName
+    String lastName
+    Double salary
 }
